5c679138833833129e21affd4dc6ecfe04f84b88
fivsevn <maxd0507@gmail.com>
2026-09-18 19:06:13 +0800

    Refresh narrative interaction localization

--- isopoda/runtime-locales.mjs
+++ isopoda/runtime-locales.mjs
@@ -1,5 +1,5 @@
 import {getLanguage} from './i18n.mjs?v=i18n-1';
-import {gameText} from './game-locales.mjs?v=interaction-story-1';
+import {gameText} from './game-locales.mjs?v=interaction-story-2';
 import {encounterById} from './encounters.mjs?v=molt-sequence-1';
 
 const selectors=[
