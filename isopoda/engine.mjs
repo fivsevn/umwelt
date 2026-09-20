eb805cbdf727b58001638c468fbc9c9d3b2ba29c
fivsevn <maxd0507@gmail.com>
2026-09-20 18:03:56 +0800

    Make runtime module routing explicit

--- isopoda/engine.mjs
+++ isopoda/engine.mjs
@@ -1,6 +1,6 @@
 import {environmentFor,changeEnvironment,ageEnvironment,environmentTarget,habitatFit,syncSceneTrace,releaseDueShells,takeShell} from './environment.mjs?v=forest-10';
 import {encounterFor,encounterById,encounterText} from './encounters.mjs?v=molt-sequence-1';
-import {SPECIES,speciesById} from './species.mjs?v=cohort-4';
+import {SPECIES,speciesById} from './species-registry.mjs?v=species-39b';
 import {EVENING,AMBIENT,CARE,MINI_TYPES,ENDINGS} from './content.mjs?v=interaction-story-2';
 export const VERSION=4;
 export const PERIODS=['晨间','午后','夜间'];
