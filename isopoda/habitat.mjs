4a6577e992d49164858033cc2599fa1471423e25
fivsevn <maxd0507@gmail.com>
2026-09-22 23:57:18 +0800

    Wire locomotion-aware behavior modules into habitat

--- isopoda/habitat.mjs
+++ isopoda/habitat.mjs
@@ -1,12 +1,12 @@
 import {habitatConfig} from './habitats.mjs';
-import {drawAquaticWater,stepAquatic} from './scenery/aquatic.mjs?v=motion-2';
+import {drawAquaticWater,stepAquatic} from './scenery/aquatic.mjs?v=locomotion-1';
 import {bindPointerInteraction} from './interaction.mjs?v=environment-memory-1';
 import {createReactions,actionFocus,drawReactionBubbles} from './reactions.mjs?v=aquatic-1';
 import {environmentFor} from './environment.mjs?v=aquatic-1';
-import {makeIndividuals,stageIndividuals,stepIndividuals} from './behaviors-speed.mjs?v=molt-sequence-1';
+import {makeIndividuals,stageIndividuals,stepIndividuals} from './behaviors-speed.mjs?v=locomotion-1';
 import {encounterById,responseMode} from './encounters.mjs?v=narrative-pool-2';
 import {pixelAnatomy,renderModel,exuviaPixels} from './sprites.mjs?v=swim-1';
-import {speciesById} from './species-registry.mjs?v=aquatic-3';
+import {speciesById} from './species-registry.mjs?v=locomotion-1';
 import {drawBaseScene,drawLeaf,DEFAULT_LAYOUT,drawSceneBackground,drawSceneElement,sceneObjects,layoutForHabitat,isAnimatedSceneElement} from './scenery/index.mjs?v=motion-2';
 import {ACTOR_SCALE} from './scenery/grammar.mjs';
 // Anatomy and scenery share the same integer world lattice.
