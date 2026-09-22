7748823c0763bfc8f9d859886000222548464ac0
fivsevn <maxd0507@gmail.com>
2026-09-22 18:22:55 +0800

    Use current isopod date locale

--- isopoda/runtime-locales.mjs
+++ isopoda/runtime-locales.mjs
@@ -1,4 +1,4 @@
-import {getLanguage} from './i18n.mjs?v=aquatic-3';
+import {getLanguage} from './i18n.mjs?v=aquatic-4';
 import {gameText} from './locales/game.mjs?v=aquatic-1';
 import {encounterById} from './encounters.mjs?v=molt-sequence-1';
 
