ccb9a8d9b6c4f93d323f971d50a7605b41e6e852
fivsevn <maxd0507@gmail.com>
2026-09-23 02:51:53 +0800

    Separate test support modules from test entry points

---
+++ tests/support/scenery-study.mjs
@@ -0,0 +1,7 @@
+// Compatibility names only. All art is implemented in the production renderer.
+export {
+ drawSubstrate as drawStudySubstrate, drawLeaf as drawStudyLeaf,
+ drawMossPatch as drawStudyMoss, drawBark as drawStudyBark,
+ drawStone as drawStudyStone, drawCuttlebone as drawStudyCuttlebone,
+ drawTwig as drawStudyTwig, drawWoodChip as drawStudyWoodChip
+} from '../../isopoda/scenery/index.mjs';
