556cf085a44081cdfce0565ca1ec1a9714478789
fivsevn <maxd0507@gmail.com>
2026-09-23 01:40:33 +0800

    Bust final abyssal story cache

--- isopoda/habitat.mjs
+++ isopoda/habitat.mjs
@@ -6,7 +6,7 @@ import {environmentFor} from './environment.mjs?v=abyssal-2';
 import {makeIndividuals,stageIndividuals,stepIndividuals} from './behaviors-speed.mjs?v=locomotion-2';
 import {encounterById,responseMode} from './encounters.mjs?v=narrative-pool-2';
 import {pixelAnatomy,renderModel,exuviaPixels} from './sprites.mjs?v=swim-2';
-import {speciesById} from './species-registry.mjs?v=abyssal-2';
+import {speciesById} from './species-registry.mjs?v=abyssal-3';
 import {drawBaseScene,drawLeaf,DEFAULT_LAYOUT,drawSceneBackground,drawSceneElement,sceneObjects,layoutForHabitat,isAnimatedSceneElement} from './scenery/index.mjs?v=motion-2';
 import {ACTOR_SCALE} from './scenery/grammar.mjs';
 // Anatomy and scenery share the same integer world lattice.
