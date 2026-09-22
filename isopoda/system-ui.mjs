751029ebd072cfb9a674c02a5710e24532011da8
fivsevn <maxd0507@gmail.com>
2026-09-23 01:46:46 +0800

    Unify runtime i18n module URL

--- isopoda/system-ui.mjs
+++ isopoda/system-ui.mjs
@@ -1,5 +1,5 @@
 import {speciesById} from './species-registry.mjs?v=aquatic-1';
-import {SUPPORTED_LANGUAGES,getLanguage,setLanguage,t,applyStaticTranslations,speciesPrimaryName} from './i18n.mjs?v=aquatic-4';
+import {SUPPORTED_LANGUAGES,getLanguage,setLanguage,t,applyStaticTranslations,speciesPrimaryName} from './i18n.mjs?v=i18n-5';
 
 const LANGS=SUPPORTED_LANGUAGES;
 const RUN_KEY='isopoda-fugue-v4';
