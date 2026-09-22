89b666b5a3933d20eb51f5b47fe6f36342846d15
fivsevn <maxd0507@gmail.com>
2026-09-22 18:18:08 +0800

    Use shared i18n module instance in runtime locale scan

--- isopoda/runtime-locales.mjs
+++ isopoda/runtime-locales.mjs
@@ -1,4 +1,4 @@
-import {getLanguage} from './i18n.mjs?v=aquatic-1';
+import {getLanguage} from './i18n.mjs?v=aquatic-3';
 import {gameText} from './locales/game.mjs?v=aquatic-1';
 import {encounterById} from './encounters.mjs?v=molt-sequence-1';
 
