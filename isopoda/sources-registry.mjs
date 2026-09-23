7b18e29e72755a60caeeaa9a13c4239ee9002928
fivsevn <maxd0507@gmail.com>
2026-09-23 19:39:03 +0800

    refactor: route sources through append manifest

--- isopoda/sources-registry.mjs
+++ isopoda/sources-registry.mjs
@@ -1,8 +1,5 @@
-import {AQUATIC_SOURCES} from './data/species/aquatic.mjs';
-import {sources as BASE_SOURCES,evidence,sourceDirectory,frameworkSources} from './sources.mjs';
-import {EXTRA_SOURCES_5} from './data/sources/marine-reference.mjs';
-import {EXTRA_SOURCES_6} from './data/sources/hobby-lines.mjs';
+import {RAW_SOURCES,evidence,sourceDirectory,frameworkSources} from './data/sources/manifest.mjs';
 
-export const sources=[...BASE_SOURCES,...EXTRA_SOURCES_5,...EXTRA_SOURCES_6,...AQUATIC_SOURCES];
+export const sources=[...RAW_SOURCES];
 export {evidence,sourceDirectory,frameworkSources};
 export const sourcesFor=species=>sources.filter(s=>species.evidenceIds?.includes(s.id));
